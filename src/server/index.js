dbf29b8d66ff62739db97e31b5650d3862a67ceb
Saif <saiofa99@gmail.com>
2022-01-08 23:32:43 +0300

    fix error

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 var path = require('path')
 const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
 var MeaningCloud = require('meaning-cloud');
 
 var meaning = MeaningCloud({
