302dccdd5d55327831baf3cb78aa64ddb4c142c5
Saif <saiofa99@gmail.com>
2022-01-08 17:23:34 +0300

    finished API request

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,63 @@
-function handleSubmit(event) {
-    event.preventDefault()
+const axios = require('axios');
 
+// get key from server.
+const getKey = async () => {
+    try {
+        const resp = await axios.get('http://localhost:8081/get-key');
+        if(resp.status === 200) {
+            // If there are no errors return key
+            return resp.data.key;
+        } else {
+            throw resp;
+        }
+    } catch (err) {
+        // Handle Error Here
+        console.error(err);
+    }
+}
+
+// Make meaning cloud API call
+const getSentiment = async (key, text) => {
+    const url = `https://api.meaningcloud.com/sentiment-2.1?key=${key}&lang=en&model=general&txt=${text}`;
+    try {
+        const resp = await axios.get(url);
+        if(resp.status === 200) {
+            // If there are no errors return data
+            return resp.data;
+        } else {
+            throw resp;
+        }
+    } catch (err) {
+        // Handle Error Here
+        console.error(err);
+    }
+}
+
+// Display results 
+const formResult = ({ agreement, confidence, irony }) => {
+    let p = document.createElement('p');
+    p.innerHTML = `Agreement: ${agreement} Confidence: ${confidence} Irony: ${irony}`;
+    document.getElementById('results').appendChild(p);
+}
+
+// An async function containing all async activity. Getting key and making API call.
+const handleAsync = async (text) => {
+    const key = await getKey();
+    const resp = await getSentiment(key, text);
+    formResult(resp);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+        
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    checkForName(formText)
+    Client.checkForName(formText)
+    
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+
+    handleAsync(formText);
 }
 
 export { handleSubmit }
--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,21 @@
+const dotenv = require('dotenv');
+dotenv.config();
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+var MeaningCloud = require('meaning-cloud');
+
+var meaning = MeaningCloud({
+    key: process.env.API_KEY
+})
 
 const app = express()
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.static('dist'))
 
@@ -14,10 +27,11 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+app.get('/get-key', function (req, res) {
+    const key = process.env.API_KEY;
+    res.send({key});
 })
